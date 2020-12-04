66968e48d48f6aeb5ffc9b472c4be743d3e4c039
Aamir Mirza <aamirshahed@hotmail.com>
2020-12-03 19:23:21 -0500

    Created a help for the command parser

--- cli/parser.py
+++ cli/parser.py
@@ -10,11 +10,11 @@ PROGRAM_DESCRIPTION = "Have a book reading goal and keep track of your " \
                       "progress"
 
 COMMANDS = (
-    'add_goal',
-    'add_book',
-    'update_goal',
-    'update_book',
-    'drop_book',
+    'add_goal', '-ag'
+                'add_book', '-ab'
+                            'update_goal', '-ug'
+                                           'update_book', '-ub'
+                                                          'drop_book', 'db'
 )
 
 
@@ -26,21 +26,24 @@ def parse_command(args):
         # No arguments passed
         # By default, recommendations should be shown
         pass
-    # TODO: Create a proper help
     # TODO: Integrate with lower classes
     parser.add_argument("Command", choices=COMMANDS, type=str,
-                        metavar="command", nargs=1, action='store')
+                        metavar="command", nargs=1, action='store',
+                        help="Choose one of the following: add_goal (-ag), "
+                             "add_book "
+                             "(-ab), update_goal (-ug), update_book (-ub), "
+                             "or drop_book (-db).")
     return parser.parse_args(args).Command[0]
 
 
 def main(args):
     if len(args) > 1:
         command = parse_command([args[1]])
-        if command == 'add_goal':
+        if command in ('add_goal', '-ag'):
             goal_parser.parse(args[2:])
-        elif command == 'add_book':
+        elif command in ('add_book', '-ab'):
             book_parser.parse(args[2:])
-        elif command == 'update_book':
+        elif command in ('update_book', '-ub'):
             # TODO: Command for update book
             pass
     recommendation.print_books()
