94621f5bb9065d04fd9548d1afae66ce3d031b2d
Aamir Mirza <aamirshahed@hotmail.com>
2020-12-03 19:17:42 -0500

    Refactored parser/created recommendation file

--- cli/parser.py
+++ cli/parser.py
@@ -1,13 +1,9 @@
 import argparse
 import sys
 
-from database.books import Books
+from cli import recommendation
 from cli import book_parser
 from cli import goal_parser
-from lib.goal_tracker import GoalTracker
-from database.goals import Goals
-from lib.shelf import Shelf
-from lib.ibook import Format
 
 PROGRAM_NAME = "Bookworm"
 PROGRAM_DESCRIPTION = "Have a book reading goal and keep track of your " \
@@ -21,8 +17,6 @@ COMMANDS = (
     'drop_book',
 )
 
-
-# TODO: Add argument for update book
 
 def parse_command(args):
     parser = argparse.ArgumentParser(prog=PROGRAM_NAME,
@@ -39,32 +33,17 @@ def parse_command(args):
     return parser.parse_args(args).Command[0]
 
 
-def get_reading_recommendations():
-    goals_db = Goals()
-    books_db = Books()
-    tracker = GoalTracker(goals_db.get_current_goal(),
-                          Shelf(books_db.get_all_books()))
-
-    opening_message = tracker.days_ahead_message()  # "You are 5 days ahead"
-    opening_message += "on your goal to "
-    opening_message += tracker.goal.message().lower()  # "read 50 books by
-    # 2021"
-    print(opening_message)
-    for recommendation in tracker.minimum_page_recommendations():
-        print(recommendation)
-    print("")
-
-
 def main(args):
-    if len(args) == 1:
-        # TODO: Print books available, or help.
-        get_reading_recommendations()
-        raise SystemExit
-    command = parse_command([args[1]])
-    if command == 'add_goal':
-        goal_parser.parse(args[2:])
-    elif command == 'add_book':
-        book_parser.parse(args[2:])
+    if len(args) > 1:
+        command = parse_command([args[1]])
+        if command == 'add_goal':
+            goal_parser.parse(args[2:])
+        elif command == 'add_book':
+            book_parser.parse(args[2:])
+        elif command == 'update_book':
+            # TODO: Command for update book
+            pass
+    recommendation.print_books()
 
 
 if __name__ == "__main__":
--- cli/recommendation.py
+++ cli/recommendation.py
@@ -1,3 +1,24 @@
-"""
+from database.books import Books
+from database.goals import Goals
+from lib.goal_tracker import GoalTracker
+from lib.shelf import Shelf
 
-"""
+EMPTY_LINE = ""
+
+
+def print_books():
+    goals_database = Goals()
+    books_database = Books()
+    tracker = GoalTracker(goals_database.get_current_goal(),
+                          Shelf(books_database.get_all_books()))
+
+    opening_message = tracker.days_ahead_message()  # "You are 5 days ahead"
+    opening_message += " on your goal to "
+    opening_message += tracker.goal.message().lower()  # "read 50 books by
+    # 2021"
+    print(EMPTY_LINE)
+    print(opening_message)
+    print(EMPTY_LINE)
+    for recommendation in tracker.minimum_page_recommendations():
+        print(recommendation)
+    print(EMPTY_LINE)
